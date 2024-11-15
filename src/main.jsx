602e1240dce608621155ef7c41bbc512d66e670b
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-15 21:26:35 +0530

    second update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 // import ReactDOM from "react-dom/client";
 // import { BrowserRouter, HashRouter } from "react-router-dom";
