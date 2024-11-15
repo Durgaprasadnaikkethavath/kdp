f4e18a43cae471fcd963e4b5f242fb29f105a41c
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-15 21:21:01 +0530

    first project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import SearchBar from "./Components/SearchBar";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Collection from "./Pages/Collection";
 import About from "./Pages/About";
@@ -13,24 +13,29 @@ import PlaceOrder from "./Pages/PlaceOrder";
 import Order from "./Pages/Order";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ShopContextProvider from "./Context/ShopContext";
 
 const App = () => {
   return (
     <>
-      <ToastContainer />
-      <Navbar />
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
-        <Route path="/order" element={<Order />} />
-      </Routes>
+      <HashRouter>
+        <ShopContextProvider>
+          <ToastContainer />
+          <Navbar />
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/place-order" element={<PlaceOrder />} />
+            <Route path="/order" element={<Order />} />
+          </Routes>
+        </ShopContextProvider>
+      </HashRouter>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import "./index.css";
-
-import { BrowserRouter, HashRouter } from "react-router-dom";
-import ShopContextProvider from "./Context/ShopContext.jsx";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, HashRouter } from "react-router-dom";
+// import ShopContextProvider from "./Context/ShopContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
-  </HashRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
