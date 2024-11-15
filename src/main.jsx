a3d20dae04706b06efb1a2b6c24f0903e813943f
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-15 19:16:22 +0530

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import ShopContextProvider from "./Context/ShopContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
